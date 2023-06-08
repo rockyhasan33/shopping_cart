cc82b2dc1842e749e2fa492ba7dc1243e16a3668
rockybul <rockyhasan403@gmail.com>
2023-06-08 19:15:24 +0530

    adding cart content

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-const body = document.body;
-const navbar = body.querySelector('.navbar');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cart = document.querySelector('.cart') 
+const cartBtn = document.querySelector('.cart-btn');
+const closeBtn = document.querySelector('.close-cart');
 
 
-window.addEventListener('scroll', (e) => {
-    if(body.screenTop > 100) {
-        alert('hello...')
+cartBtn.addEventListener('click',() => {
+    if(cartOverlay.classList.contains('overlay-visible')) {
+        cartOverlay.classList.remove('overlay-visible');
+    } else {
+        cartOverlay.classList.add('overlay-visible')
     }
+
+    if(cart.classList.contains('cart-collapse')) {
+        cart.classList.remove('cart-collapse');
+    } else {
+        cart.classList.add('cart-collapse')
+    }
+})
+
+closeBtn.addEventListener('click', () => {
+    cart.classList.remove('cart-collapse');
+    cartOverlay.classList.remove('overlay-visible');
 })
 
